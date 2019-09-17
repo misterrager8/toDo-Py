e1ea4710ee536e9dde7a221b3af953afe4c17863
Chemllei Joseph <chemllei0304@gmail.com>
2019-09-16 22:24:16 -0400

    160919 - export to txt

--- toDo.py
+++ toDo.py
@@ -1,5 +1,6 @@
 import MySQLdb
 from datetime import datetime
+import os
 
 date_now = datetime.now().strftime("%m/%d/%Y")
 
@@ -24,8 +25,7 @@ def viewItems():
     for row in results:
       itemid = row[0]
       item = row[1]
-      dateadded = row[2]
-      print "%d\t%s\t%s" % (itemid, item, dateadded)
+      print "%d\t%s" % (itemid, item)
   except MySQLdb.Error, e:
     print(e)
   db.close()
@@ -40,20 +40,40 @@ def deleteItem(itemid):
   except MySQLdb.Error, e:
     print(e)
   db.close()
-
+  
+def writeToFile(itemid, item):
+  fo = open(os.getcwd() + "todo.txt", "a")
+  fo.write("%d\t%s\n" % (itemid, item))
+  fo.close()
+  
+def exportTXT():
+  db = MySQLdb.connect("localhost","root","bre9ase4","TESTDB")
+  cursor = db.cursor()
+  sql = "SELECT * FROM todolist"
+  try:
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for row in results:
+      itemid = row[0]
+      item = row[1]
+      writeToFile(itemid, item)
+  except MySQLdb.Error, e:
+    print(e)
+  db.close()
+  
 while True:
-  answer = raw_input("(A)View Items\t(B)Add Item\t(C)Delete Item\t(D)Exit\n")
+  print("\t____TO-DO LIST____")
+  viewItems()
+  answer = raw_input("\n(A)Add Item\n(B)Delete Item\n(C)Export To TXT\n(D)Exit")
   if answer == "a":
-    viewItems()
-  elif answer == "b":
     itemName = raw_input("Item Name: ")
     addItem(itemName, date_now)
     print("Item Added.")
-    viewItems()
-  elif answer == "c":
+  elif answer == "b":
     itemID = raw_input("Item ID: ")
     deleteItem(int(itemID))
     print("Item Deleted.")
-    viewItems()
+  elif answer == "c":
+    exportTXT()
   elif answer == "d":
     exit()
