b12498510aafed4f221a03efeb877285cc176fe9
Chemllei Joseph <chemllei0304@gmail.com>
2021-07-25 23:17:12 -0400

    Refactor views

--- modules/view.py
+++ modules/view.py
@@ -1,93 +1,196 @@
-from flask import render_template, request, redirect, url_for
+import random
+
+from flask import render_template, url_for, request
+from sqlalchemy import text
+from werkzeug.utils import redirect
 
 from modules import app, db
-from modules.ctrla import ToDoDB
-from modules.model import Task, Folder
+from modules.model import Task, Folder, Habit, List
 
-task_db = ToDoDB()
+
+@app.context_processor
+def inject_all():
+    all_folders = db.session.query(Folder).order_by(text("date_created desc")).all()
+    return dict(all_folders=all_folders)
 
 
 @app.route("/")
 def index():
-    order_by = request.args.get("order_by", default="date_created desc")
-
-    folders = task_db.get_all(Folder, order_by=order_by)
-    return render_template("index.html", folders=folders, order_by=order_by)
+    return render_template("index.html")
 
 
-@app.route("/folder", methods=["POST", "GET"])
-def folder_items():
-    order_by = request.args.get("order_by", default="date_added desc")
+@app.route("/tasks")
+def tasks():
+    order_by = request.args.get("order_by", default="tasks.date_created desc")
+    return render_template("tasks.html", tasks_=db.session.query(Task).join(Folder).order_by(text(order_by)).all(), order_by=order_by)
+
+
+@app.route("/task_create", methods=["POST"])
+def task_create():
+    db.session.add(Task(name=request.form["name"].title(),
+                        priority=int(request.form["priority"]),
+                        note=request.form["note"],
+                        folder=int(request.form["folder"])))
+    db.session.commit()
+
+    return redirect(url_for("tasks"))
+
+
+@app.route("/task_update", methods=["POST"])
+def task_update():
     id_: int = request.args.get("id_")
-    folder: Folder = task_db.find_by_id(Folder, id_)
+    _: Task = db.session.query(Task).get(id_)
 
-    return render_template("folder_items.html", folder=folder, order_by=order_by)
+    _.name = request.form["name"]
+    _.priority = int(request.form["priority"])
+    _.note = request.form["note"]
+    _.folder = int(request.form["folder"])
+    db.session.commit()
+
+    return redirect(url_for("tasks"))
+
+
+@app.route("/task_delete")
+def task_delete():
+    id_: int = request.args.get("id_")
+    _: Task = db.session.query(Task).get(id_)
+
+    db.session.delete(_)
+    db.session.commit()
+
+    return redirect(url_for("tasks"))
+
+
+@app.route("/task_toggle")
+def task_toggle():
+    id_: int = request.args.get("id_")
+    _: Task = db.session.query(Task).get(id_)
+
+    db.session.commit()
+
+    return redirect(url_for("tasks"))
+
+
+@app.route("/folders")
+def folders():
+    order_by = request.args.get("order_by", default="date_created desc")
+    return render_template("folders.html", folders_=db.session.query(Folder).order_by(text(order_by)).all(), order_by=order_by)
 
 
 @app.route("/folder_create", methods=["POST"])
 def folder_create():
-    folder_name = request.form["folder_name"].title()
-    folder_color = request.form["color"]
-
-    db.session.add(Folder(folder_name, color=folder_color))
+    db.session.add(Folder(name=request.form["name"].title(),
+                          color="#{:06x}".format(random.randint(0, 0xFFFFFF))))
     db.session.commit()
 
-    return redirect(url_for("index"))
+    return redirect(url_for("folders"))
 
 
 @app.route("/folder_update", methods=["POST"])
 def folder_update():
     id_: int = request.args.get("id_")
-    folder_: Folder = db.session.query(Folder).get(id_)
+    _: Folder = db.session.query(Folder).get(id_)
 
-    folder_.name = request.form["folder_name"]
-    folder_.color = request.form["color"]
+    _.name = request.form["name"]
+    _.color = request.form["color"]
     db.session.commit()
 
-    return redirect(url_for("index"))
+    return redirect(url_for("folders"))
 
 
 @app.route("/folder_delete")
 def folder_delete():
     id_: int = request.args.get("id_")
-    task_db.delete_one(task_db.find_by_id(Folder, id_))
+    _: Folder = db.session.query(Folder).get(id_)
 
-    return redirect(url_for("index", folders=task_db.get_all(Folder)))
+    db.session.delete(_)
+    db.session.commit()
+
+    return redirect(url_for("folders"))
 
 
-@app.route("/task_create", methods=["POST"])
-def task_create():
+@app.route("/habits")
+def habits():
+    return render_template("habits.html", habits_=db.session.query(Habit).all())
+
+
+@app.route("/habit_create", methods=["POST"])
+def habit_create():
+    db.session.add(Habit(name=request.form["name"].title(),
+                         frequency=request.form["frequency"],
+                         color="#{:06x}".format(random.randint(0, 0xFFFFFF))))
+    db.session.commit()
+
+    return redirect(url_for("habits"))
+
+
+@app.route("/habit_update", methods=["POST"])
+def habit_update():
     id_: int = request.args.get("id_")
-    folder: Folder = task_db.find_by_id(Folder, id_)
+    _: Habit = db.session.query(Habit).get(id_)
 
-    task_title = request.form["task_title"]
-    task_notes = request.form["task_notes"]
-    priority = request.form["priority"]
+    _.name = request.form["name"]
+    _.frequency = request.form["frequency"]
+    _.color = request.form["color"]
+    db.session.commit()
 
-    folder.add_task(Task(task_title, notes=task_notes, priority=priority))
-    return redirect(url_for("folder_items", id_=folder.id))
+    return redirect(url_for("habits"))
 
 
-@app.route("/delete_task")
-def delete_task():
+@app.route("/habit_delete")
+def habit_delete():
     id_: int = request.args.get("id_")
-    order_by = request.args.get("order_by")
-    task: Task = task_db.find_by_id(Task, id_)
-    folder_id = task.folder_id
+    _: Habit = db.session.query(Habit).get(id_)
 
-    task_db.delete_one(task)
+    db.session.delete(_)
+    db.session.commit()
 
-    return redirect(url_for("folder_items", id_=folder_id, order_by=order_by))
+    return redirect(url_for("habits"))
 
 
-@app.route("/toggle_done")
-def toggle_done():
-    id_ = int(request.args.get("id_"))
-    order_by = request.args.get("order_by")
-    _: Task = task_db.find_by_id(Task, id_)
-    if _.done:
-        _.toggle_done(False)
-    else:
-        _.toggle_done(True)
+@app.route("/habit_today")
+def habit_today():
+    id_: int = request.args.get("id_")
+    _: Habit = db.session.query(Habit).get(id_)
 
-    return redirect(url_for("folder_items", id_=_.folders.id, order_by=order_by))
+    db.session.commit()
+
+    return redirect(url_for("habits"))
+
+
+@app.route("/lists")
+def lists():
+    order_by = request.args.get("order_by", default="date_created desc")
+    return render_template("lists.html", lists_=db.session.query(List).order_by(text(order_by)).all(), order_by=order_by)
+
+
+@app.route("/list_create")
+def list_create():
+    db.session.add(List(name=request.form["name"],
+                        contents=request.form["contents"]))
+    db.session.commit()
+
+    return redirect(url_for("lists"))
+
+
+@app.route("/list_update", methods=["POST"])
+def list_update():
+    id_: int = request.args.get("id_")
+    _: List = db.session.query(List).get(id_)
+
+    _.name = request.form["name"]
+    _.contents = request.form["contents"]
+    db.session.commit()
+
+    return redirect(url_for("lists"))
+
+
+@app.route("/list_delete")
+def list_delete():
+    id_: int = request.args.get("id_")
+    _: List = db.session.query(List).get(id_)
+
+    db.session.delete(_)
+    db.session.commit()
+
+    return redirect(url_for("lists"))
