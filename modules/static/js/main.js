f89be738b1e47499329ec92cdafb9ad5a99bb778
Chemllei Joseph <chemllei0304@gmail.com>
2021-10-09 16:43:06 -0400

    Add alternate color theme

--- modules/static/js/main.js
+++ modules/static/js/main.js
@@ -2,3 +2,11 @@ function addInput(elem) {
     var nput = elem.previousElementSibling.cloneNode();
     elem.parentNode.insertBefore(nput, elem);
 }
+
+function changeTheme() {
+    if (document.getElementById("site-content").classList.contains("other-theme")) {
+        document.getElementById("site-content").classList.remove("other-theme");
+    } else {
+        document.getElementById("site-content").classList.add("other-theme");
+    }
+}
