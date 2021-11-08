42314cf02ad398c2de913e1bdf618432b6203680
Chemllei Joseph <chemllei0304@gmail.com>
2021-11-08 15:52:20 -0500

    Remove masonry implem.

--- modules/static/js/main.js
+++ modules/static/js/main.js
@@ -8,11 +8,6 @@ function changeTheme() {
 
 function expandDiv(parentId, divId) {
     $('#' + divId).fadeToggle();
-    if ($('#' + divId).css('display') == 'none') {
-        $('#' + parentId).css('z-index', 1);
-    } else {
-        $('#' + parentId).css('z-index', 2);
-    }
 }
 
 function checkBox(elem, id) {
