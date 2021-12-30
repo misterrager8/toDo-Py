f06524a4ba2ddb9037ae4b5c70a277e0076ce55c
Chemllei Joseph <chemllei0304@gmail.com>
2021-12-30 03:01:50 -0500

    Config tweaks

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ import dotenv
 
 dotenv.load_dotenv()
 
+FLASK_ENV = os.getenv("flask_env")
 SQLALCHEMY_DATABASE_URI = os.getenv("db_url")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': 60}
 SECRET_KEY = os.getenv("secret_key")
-DEBUG = True
+DEBUG = os.getenv("debug")
