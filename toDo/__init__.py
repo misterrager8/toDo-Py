cea52450f04708bc39db6ac6200579026cd72912
Chemllei Joseph <chemllei0304@gmail.com>
2022-03-14 19:26:51 -0400

    Add SCSS, tweak styles

--- toDo/__init__.py
+++ toDo/__init__.py
@@ -1,6 +1,7 @@
 import pymysql
 from flask import Flask
 from flask_login import LoginManager
+from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
 pymysql.install_as_MySQLdb()
@@ -13,6 +14,7 @@ def create_app(config):
     app.config.from_object(config)
     db.init_app(app)
     login_manager.init_app(app)
+    Scss(app, asset_dir="toDo/static")
 
     with app.app_context():
         from . import views
